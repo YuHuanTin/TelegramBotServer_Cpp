c7a95a6c315a9e5f1246c22b678a51105a76a6f0
23945 <2394555023@qq.com>
2022-08-19 13:57:54 +0800

    ;

---
+++ WinServerWithLibEvent/WinServerWithLibEvent.cpp
@@ -0,0 +1,11 @@
+#include "WinServerWithLibEvent.h"
+
+using std::string;
+using std::vector;
+
+int main(){
+    event_base *eventBase = event_base_new();
+
+
+    return 0;
+}
---
+++ WinServerWithLibEvent/WinServerWithLibEvent.h
@@ -0,0 +1,9 @@
+#ifndef LinuxServerWithLibEvent_H
+#define LinuxServerWithLibEvent_H
+#include <string>
+#include <vector>
+#include <event.h>
+#include <event2/http.h>
+#include <event2/http_struct.h>
+
+#endif //LinuxServerWithLibEvent_H
